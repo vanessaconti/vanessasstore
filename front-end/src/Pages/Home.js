111747538ce65696af7cbe0a92ee31d318bad0a4
vanessaconti <vconti901@aol.com>
2022-02-24 23:56:57 -0500

    forgot to add maintenance message

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -4,6 +4,16 @@ function Home() {
       <div>
         <h1>Welcome to Vanessas LOL Surprise Doll World Store!! App</h1>
         <h2>Created by Vanessa Conti </h2>
+        <h3>
+          WE ARE ADDING MORE COLLECTIONS DAILY... PLEASE CHECK BACK FREQUENTLY
+          FOR UPDATES
+        </h3>
+
+        <h3>
+          {" "}
+          Under new management.. Please excuse our appearance while we are under
+          maintenance.
+        </h3>
       </div>
     </>
   );
